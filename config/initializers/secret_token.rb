7f956627861ed989af335c7862b04217aad6448c
Rodrigo C. Andrade <rodrigoandradebcc@gmail.com>
2014-06-04 17:29:02 -0300

    teste

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TiagoBelemV2::Application.config.secret_key_base = '887d4529a6bc56b5e47b6f2dcb16f2a1e752dbe8023e18f729134e322b8557b158a8dc2a1541b56cc239d93cdd3c0ac0cb55a42c4eca9c0fd23d13ab5bc7b738'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TiagoBelemV2::Application.config.session_store :cookie_store, key: '_tiago_belem_v2_session'

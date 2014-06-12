2579d8c247ef4c6fbb77026dd19852e934658379
Rodrigo C. Andrade <rodrigoandradebcc@gmail.com>
2014-06-12 18:36:53 -0300

    Novo site

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+  	@movies = Movie.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 TiagoBelemV2::Application.routes.draw do
+  get "welcome/index"
   resources :movies
 
   resources :endorsees
 
   resources :bios
+
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

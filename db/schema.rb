617b73ba29f52d34002e2091945abb5342e42bc2
Rodrigo C. Andrade <rodrigoandradebcc@gmail.com>
2014-07-30 18:18:00 -0300

    Atualizando

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730014307) do
+ActiveRecord::Schema.define(version: 20140730150108) do
 
   create_table "bios", force: true do |t|
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

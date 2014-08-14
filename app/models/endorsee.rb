55ae6baebadfed8170c5b959605ce9a5aed38949
Rodrigo Andrade <rodrigoandradebcc@gmail.com>
2014-08-14 16:58:14 -0300

    teste

--- app/models/endorsee.rb
+++ app/models/endorsee.rb
@@ -1,2 +1,3 @@
 class Endorsee < ActiveRecord::Base
+	mount_uploader :image, ImageUploader
 end
--- db/migrate/20140612141101_create_endorsees.rb
+++ db/migrate/20140612141101_create_endorsees.rb
@@ -2,6 +2,7 @@ class CreateEndorsees < ActiveRecord::Migration
   def change
     create_table :endorsees do |t|
       t.string :link
+      t.string :image
 
       t.timestamps
     end

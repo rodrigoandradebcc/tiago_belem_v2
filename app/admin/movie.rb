3ff4247d2c7fe9ad40021e5efc41c4eaa488f796
Rodrigo C. Andrade <rodrigoandradebcc@gmail.com>
2014-08-18 14:36:45 -0300

    design

---
+++ app/admin/movie.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register Movie do
+  permit_params :link
+  index do
+    column "Link do Vídeo", :link
+    actions
+  end
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #  permitted = [:permitted, :attributes]
+  #  permitted << :other if resource.something?
+  #  permitted
+  # end
+
+
+end

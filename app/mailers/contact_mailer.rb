6d69b4658e00529092c299dfc178aa0f329c933f
Rodrigo Andrade <rodrigoandradebcc@gmail.com>
2014-09-13 18:43:16 -0300

    Deploy

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,12 +2,12 @@ class ContactMailer < ActionMailer::Base
   def contact_data(contact)
     @contact = contact
 
-    mail to: 'rodrigoandradebc@gmail.com', from: @contact.email, subject: 'SITE: CONTATO TIAGO'
+    mail to: 'rodrigoandradebcc@gmail.com', from: @contact.email, subject: 'SITE: CONTATO TIAGO'
   end
 
   def thanks(contact)
     @contact = contact
 
-    mail to: contact.email, from: 'rodriguinhocarneiroandrade@hotmail.com', subject: 'Obrigado, em breve vamos entrar em contato!'
+    mail to: contact.email, from: 'rodriguinhocarneiroandrade@hotmail.com', subject: 'Obrigado por entrar em contato!'
   end
 end
